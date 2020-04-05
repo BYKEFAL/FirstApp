c820f08c319f168f13e9d773a4cca7386881e89f
BYKEFAL <oleg-expert@mail.ru>
2020-04-05 17:14:51 +0300

    изменения в puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -10,10 +10,11 @@ environment ENV['RACK_ENV'] || 'development'
 on_worker_boot do
     # Настройки, специфические для Rails 4.1+ см.: https://devcenter.heroku.com/articles/
     # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
-    ActiveSupport.on_load(:active_record) do
-        config = ActiveRecord::Base.configurations[Rails.env] ||
-                    Rails.application.config.database_configuration[Rails.env]
-        config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
-        ActiveRecord::Base.establish_connection(config)
-      end
+    ActiveRecord::Base.establish_connection
+    #ActiveSupport.on_load(:active_record) do
+        #config = ActiveRecord::Base.configurations[Rails.env] ||
+                   # Rails.application.config.database_configuration[Rails.env]
+     #   config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
+     #   ActiveRecord::Base.establish_connection(config)
+     # end
 end
